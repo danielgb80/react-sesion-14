b3509a331a95aee917b8be854449b85597e1654a
Daniel GB <danielgb_80@hotmail.com>
2023-11-11 12:06:06 -0600

    ejercicio 1 sesion 14

--- src/App.jsx
+++ src/App.jsx
@@ -2,53 +2,25 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Loader from './components/loader/Loader'
 
 function App() {
-  const [count, setCount] = useState(0)
-  const [casa, setCasa] = useState("Casa amarilla");
-  const [alumno, setAlumno] = useState("Daniel");
-  const [alumnos, setAlumnos] = useState([1,2,3,4]);
-
-  const eventoClick = () =>{
-    setCount((count) => count + 1);
-    setAlumno("Javier");
-    setAlumnos([
-      "Javier",
-      "Jorge",
-      "Diana"
-    ]);
-  
-  }
-
-  return (
+const[loading, setLoading] = useState(true);
+//{Loading && <Loader/>}
+setTimeout(()=>{
+  setLoading(false);
+}, 3000);  
+return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => eventoClick()}>
-          el valor de count es: {count}
-        </button>
-        <p>
-          {casa} Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-        <p>{alumno}</p>
-        <code>
-
-{alumnos.map((alumno)=><p>{alumno}</p>)}
-
-</code>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    {
+    loading ?
+    <Loader/> : 
+    <>
+       <h1>Vite + React sesión 14 </h1>
+      <button className='btn btn-primary'> Click </button>
+      </>
+}
+</>
   )
 }
 
---
+++ src/components/loader/Loader.jsx
@@ -0,0 +1,10 @@
+import './loader.css';
+function loader() {
+    return (
+        <>
+        <div class="lds-ripple"><div></div><div></div></div>
+        <p>Cargando...</p>
+        </>
+    )
+}
+export default loader;
